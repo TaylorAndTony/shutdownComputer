553614053b047176060f9c252a1882565966bb16
Taylor-PC <doyijian@163.com>
2021-02-01 18:36:09 +0800

    just a gui

--- test.py
+++ test.py
@@ -1,35 +1,35 @@
 from tkinter import *
 from tkinter import ttk
 
-
-class PopoutWindow:
-    def __init__(self, imgFile, title, message):
-        self.llayout = {'padx': 10, 'pady': 10}
+class InputBox:
+    """
+    一个用于获取用户输入的输入框，用法如下：
+    ib = InputBox('hello', '请输入信息：')
+    print(ib.result)
+    """
+    def __init__(self, title, description):
         self.root = Tk()
         self.root.title(title)
-        # self.root['title'] = title
-        self.message = message
-        self.photo = PhotoImage(file=imgFile)
-        self.leftSide = Frame(self.root)
-        self.rightSide = Frame(self.root)
-        self.place_main()
-        self.place_button()
+        self.desc = description
+        self.entry = Entry(self.root, width=40)
+        self.button = ttk.Button(self.root, text='确定', command=self.getText)
+        self.result = ''
+        self.layout()
         self.run()
+    
+    def layout(self):
+        Label(self.root, text=self.desc).pack(padx=10, pady=10)
+        self.entry.pack(padx=10, pady=10)
+        self.button.pack(padx=10, pady=10)
 
-    def place_main(self):
-        Label(self.leftSide, image=self.photo).pack(**self.llayout)
-        Label(self.rightSide, text=self.message
-              ).pack(**self.llayout)
-
-    def place_button(self):
-        ttk.Button(self.root, text='确认', command=lambda: quit()
-                   ).grid(row=1, columnspan=2, **self.llayout)
+    def getText(self):
+        self.result = self.entry.get()
+        self.root.destroy()
 
     def run(self):
-        self.leftSide.grid(row=0, column=0)
-        self.rightSide.grid(row=0, column=1)
         self.root.mainloop()
 
 
 if __name__ == '__main__':
-    PopoutWindow('./img/trash.png', '清空', '已清空选择列表')
+    ib = InputBox('hello', '请输入信息：')
+    print(ib.result)
